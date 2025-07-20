649f6e942d7bb2924e10fcd62bbce10fec3cc086
Bama2262 <bamamaulana2036@gmail.com>
2025-07-20 09:21:33 +0700

    fix main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QLabel,
-                                                       QPushButton, QVBoxLayout, QHBoxLayout)
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout)
 from PyQt5.QtCore import QTimer, QTime, Qt
 
 class Stopwatch(QWidget):
