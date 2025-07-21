c48492000dee9439813d159f5d8fe97ed48da73f
Bama2262 <bamamaulana2036@gmail.com>
2025-07-22 04:57:09 +0700

    fix main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Stopwatch(QWidget):
     def __init__(self):
         super().__init__()
         self.time = QTime(0, 0, 0, 0)
-        self.time_label = QLabel("00:00:00.00", self)
+        self.time_label = QLabel("00:00:00:00", self)
         self.start_button = QPushButton("Start", self)
         self.stop_button = QPushButton("Stop", self)
         self.reset_button = QPushButton("Reset", self)
